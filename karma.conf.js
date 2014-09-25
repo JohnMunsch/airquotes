f5e81e77eb7e25d73e9bfd831f2c38b9e771ff86
John Munsch <john.munsch@gmail.com>
2014-09-25 15:38:05 -0500

    Fixed broken unit tests caused by my AngularJS upgrade.

--- karma.conf.js
+++ karma.conf.js
@@ -16,6 +16,7 @@ module.exports = function(config) {
     files: [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/angular-route/angular-route.js',
       'app/scripts/**/*.js',
       'test/**/*Spec.js'
     ],
